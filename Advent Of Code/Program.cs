46fd9d1bd1cf8d887814fd4fc2d87f35a5a182c2
Joel Brunnegård <joel.brunnegard@edu.futuregames.nu>
2022-12-01 14:48:34 +0100

    day 1.1

--- Advent Of Code/Program.cs
+++ Advent Of Code/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var f = File.OpenText("data.txt");
+
+int maxElfAmount = 0;
+int currentElfAmount = 0;
+
+while (!f.EndOfStream)
+{
+	var line = f.ReadLine();
+	if (line == "")
+	{
+		if (maxElfAmount < currentElfAmount)
+			maxElfAmount = currentElfAmount;
+		currentElfAmount = 0;
+	}
+	else
+	{
+		currentElfAmount += int.Parse(line);
+	}
+}
+
+Console.WriteLine(maxElfAmount);
