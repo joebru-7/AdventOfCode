34381d25ef2aab53420d9983add1c65a3ffea450
Joel Brunnegård <joel.brunnegard@gmail.com>
2022-12-02 15:10:15 +0100

    added funcions to keep both parts

--- day2/Program.cs
+++ day2/Program.cs
@@ -1,32 +1,42 @@
-﻿var f = File.OpenText("data.txt");
+﻿part1();
+part2();
 
-int score = 0;
-
-while (!f.EndOfStream)
+void part1()
 {
-	var line = f.ReadLine();
 
-	char[] parts = new[] { line[0], line[2] };
-
-	//score for win
-	score += (parts[1] - 'X') *3;
-
-
-
-	int choice =  ((parts[0] - 'A') + (parts[1] - 'X'-1)+3)%3;
-
-	switch(choice)
-	{
-		case 0: //rock A
-			score+= 1;
-			break;
-		case 1:// paper B
-			score += 2;
-			break;
-		case 2: //sissors C
-			score += 3;
-			break;
-	}
 }
 
-Console.WriteLine(score);
+void part2()
+{
+	var f = File.OpenText("data.txt");
+
+	int score = 0;
+
+	while (!f.EndOfStream)
+	{
+		var line = f.ReadLine();
+
+		char[] parts = new[] { line[0], line[2] };
+
+		//score for win
+		score += (parts[1] - 'X') * 3;
+
+		int choice = ((parts[0] - 'A') + (parts[1] - 'X' - 1) + 3) % 3;
+
+		switch (choice)
+		{
+			case 0: //rock A
+				score += 1;
+				break;
+			case 1:// paper B
+				score += 2;
+				break;
+			case 2: //sissors C
+				score += 3;
+				break;
+		}
+	}
+
+	Console.WriteLine("part2 score: " + score);
+	f.Close();
+}
