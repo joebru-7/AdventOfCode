2039c455928a66c62e36a9b417794e290ceee343
Joel Brunnegård <joel.brunnegard@home.se>
2022-12-09 11:20:33 +0100

    start day9

---
+++ day9/day9.cs
@@ -0,0 +1,34 @@
+﻿part1();
+part2();
+
+void part1()
+{
+	var f = File.OpenText("data.txt");
+
+	int score = 0;
+
+	while (!f.EndOfStream)
+	{
+		var line = f.ReadLine();
+	}
+
+	Console.WriteLine("part1 : " + score);
+	f.Close();
+}
+
+void part2()
+{
+
+	var f = File.OpenText("data.txt");
+
+	int score = 0;
+
+	while (!f.EndOfStream)
+	{
+		var line = f.ReadLine();
+	}
+
+
+	Console.WriteLine("part2 : " + score);
+	f.Close();
+}
